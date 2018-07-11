6cb6b1e2624490f9c312ccc3ae9b3d0f1b6e5689
Ben <bmh16e@my.fsu.edu>
2018-07-11 16:16:53 -0400

    Login fragment added and being setup

--- app/src/main/java/fsu/mobile/group1/geohashing/MainActivity.java
+++ app/src/main/java/fsu/mobile/group1/geohashing/MainActivity.java
@@ -1,13 +1,29 @@
 package fsu.mobile.group1.geohashing;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoginFragment.LoginListener {
+
+    private FragmentManager mManager;
+    private FragmentTransaction fragTransaction;
 //this is a comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayLogin();
+    }
+
+
+    //add login fragment to ui
+    public void displayLogin(){
+        LoginFragment loginFragment=new LoginFragment();
+        mManager=getSupportFragmentManager();
+        fragTransaction=mManager.beginTransaction();
+        fragTransaction.add(R.id.login_fragment, loginFragment, "login_fragment");
+        fragTransaction.commit();
     }
 }
