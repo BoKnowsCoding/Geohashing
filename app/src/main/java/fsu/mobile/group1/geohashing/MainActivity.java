dcc8859749fe516ff72828d52cb9a69fe9cb5044
Atanimulli <jrb14b@my.fsu.edu>
2018-07-11 18:44:21 -0400

    Added in permissions we need, there may be more I didn't think of

--- app/src/main/java/fsu/mobile/group1/geohashing/MainActivity.java
+++ app/src/main/java/fsu/mobile/group1/geohashing/MainActivity.java
@@ -4,13 +4,19 @@ package fsu.mobile.group1.geohashing;
 * https://stackoverflow.com/questions/32671004/how-to-change-the-color-of-a-button-in-android-studio/32671191
 * */
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity implements LoginFragment.LoginListener {
-
+    //Permission Ids
+    static final int MY_PERMISSIONS_REQUEST_INTERNET = 432, MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE = 761
+            ,MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 123, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 7;
     private FragmentManager mManager;
     private FragmentTransaction fragTransaction;
 //this is a comment
@@ -18,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Log
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermissions();
         displayLogin();
     }
 
@@ -38,5 +45,34 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Log
     }
     public void onGoogleSignIn(){
 
+    }
+
+    //Gonna programmatically check for permissions
+    public void checkPermissions()
+    {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_NETWORK_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_NETWORK_STATE},
+                    MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.INTERNET},
+                    MY_PERMISSIONS_REQUEST_INTERNET);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
     }
 }
