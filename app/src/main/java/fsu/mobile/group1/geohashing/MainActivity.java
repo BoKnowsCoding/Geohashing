7dbb5f4610492de1f670c34e32eebe9fcedc3f31
Brandon Hahn <blh16b@my.fsu.edu>
2018-07-12 20:41:05 -0400

    Set initial location to user's current location when MapsActivity is started

--- app/src/main/java/fsu/mobile/group1/geohashing/MainActivity.java
+++ app/src/main/java/fsu/mobile/group1/geohashing/MainActivity.java
@@ -16,6 +16,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -113,7 +114,8 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Log
             startActivity(intent);
         }
         catch (ApiException e){
-                    }
+            Log.i("MainActivity", "Exception");
+            }
 
     }
 
--- app/src/main/java/fsu/mobile/group1/geohashing/MapsActivity.java
+++ app/src/main/java/fsu/mobile/group1/geohashing/MapsActivity.java
@@ -1,8 +1,16 @@
 package fsu.mobile.group1.geohashing;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,7 +19,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private GoogleMap mMap;
 
     @Override
@@ -37,10 +45,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            // that weirdly long variable is just a result code, declared up top
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
+            // I believe this sets the location to wherever the user is, and follows them unless they move the map.
+            mMap.setMyLocationEnabled(true);
+        }
+        // Original code in here:
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
