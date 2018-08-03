dde00bc18d8fcdaf02341d6635c99244ef970461
Ben <bmh16e@my.fsu.edu>
2018-08-03 15:25:39 -0400

    Additional updates to the UI as well as navigational features.

--- app/src/main/java/fsu/mobile/group1/geohashing/themeFragment.java
+++ app/src/main/java/fsu/mobile/group1/geohashing/themeFragment.java
@@ -1,13 +1,19 @@
 package fsu.mobile.group1.geohashing;
 
 
+import android.app.ActionBar;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.RadioButton;
 
@@ -24,7 +30,7 @@ public class themeFragment extends Fragment implements View.OnClickListener {
    private RadioButton red;
    private Button apply;
    private ThemeListener themeListener;
-
+   private Button home;
 
     public themeFragment() {
         // Required empty public constructor
@@ -32,6 +38,8 @@ public class themeFragment extends Fragment implements View.OnClickListener {
 
     public interface ThemeListener{
         public void reloadTheme();
+        public void toolbarChange(String color);
+        public void home();
     }
 
     @Override
@@ -40,12 +48,25 @@ public class themeFragment extends Fragment implements View.OnClickListener {
 
         // Inflate the layout for this fragment
         View root= inflater.inflate(R.layout.fragment_theme, container, false);
+
         dark=root.findViewById(R.id.dark_id);
         green=root.findViewById(R.id.green_id);
         blue=root.findViewById(R.id.blue_id);
         red=root.findViewById(R.id.red_id);
         apply=root.findViewById(R.id.apply_id);
+        home=root.findViewById(R.id.home_id);
+        home.setOnClickListener(this);
         apply.setOnClickListener(this);
+
+/*
+       tools=root.findViewById(R.id.action_bar);
+
+        ((AppCompatActivity)getActivity()).setSupportActionBar(tools);
+       ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(false);
+        tools.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
+*/
+
+
         return root;
     }
 
@@ -54,37 +75,68 @@ public class themeFragment extends Fragment implements View.OnClickListener {
 
         View root=v.getRootView();
 
-        if(dark.isChecked()){
-            Log.i("dark", "dark theme selected.............................");
+        Window window;
+
+
+if(v==apply) {
+    if (dark.isChecked()) {
         getContext().setTheme(R.style.Blackout_Base);
         dark.setChecked(false);
         root.setBackgroundColor(getResources().getColor(R.color.foreground_materiallight));
-        themeListener.reloadTheme();
+
+        if (getActivity().getWindow() != null) {
+            window = getActivity().getWindow();
+            themeListener.toolbarChange("Dark");
+            themeListener.reloadTheme();
+            window.setStatusBarColor(getResources().getColor(R.color.black));
         }
-        else if(green.isChecked()){
-            getContext().setTheme(R.style.Green_Base);
-            green.setChecked(false);
-            root.setBackgroundColor(getResources().getColor(R.color.colorBackground));
+    } else if (green.isChecked()) {
+        getContext().setTheme(R.style.Green_Base);
+        green.setChecked(false);
+
+        root.setBackgroundColor(getResources().getColor(R.color.colorBackground));
+        if (getActivity().getWindow() != null) {
+            window = getActivity().getWindow();
+            themeListener.toolbarChange("Green");
             themeListener.reloadTheme();
 
+            window.setStatusBarColor(getResources().getColor(R.color.primary_materiallight));
+
         }
-        else if (blue.isChecked()){
+
+    } else if (blue.isChecked()) {
         getContext().setTheme(R.style.Blue_Base);
         blue.setChecked(false);
-            root.setBackgroundColor(getResources().getColor(R.color.colorBackground));
+        root.setBackgroundColor(getResources().getColor(R.color.colorBackground));
 
+        if (getActivity().getWindow() != null) {
+            window = getActivity().getWindow();
+            themeListener.toolbarChange("Blue");
             themeListener.reloadTheme();
 
+            window.setStatusBarColor(getResources().getColor(R.color.holo_blueDark));
+
         }
-        else if (red.isChecked()){
+
+    } else if (red.isChecked()) {
         getContext().setTheme(R.style.Red_Base);
         red.setChecked(false);
-            root.setBackgroundColor(getResources().getColor(R.color.colorBackground));
+        root.setBackgroundColor(getResources().getColor(R.color.colorBackground));
 
-
+        if (getActivity().getWindow() != null) {
+            window = getActivity().getWindow();
+            themeListener.toolbarChange("Red");
             themeListener.reloadTheme();
+            window.setStatusBarColor(getResources().getColor(R.color.primary_materialdark));
 
         }
+
+
+    }
+}
+        else if(v==home){
+        themeListener.home();
+            }
     }
 
     @Override
